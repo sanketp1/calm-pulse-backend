e0d78faea18fbe0a147b3cb3e602f2a00a4ec26b
Sanket Patil <psanket18052001@gmail.com>
2025-05-23 15:53:41 +0530

    added user repo with few custom methods

---
+++ src/main/java/com/neocortex/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.neocortex.repositories;
+
+import com.neocortex.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existByEmail(String email);
+
+}
